f99d5993edee23c50700ab862ed1d5977c66632f
Aida Sertovic <sertovicaida@gmail.com>
2021-05-17 19:54:49 +0200

    Remove items from cart

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector} from 'reselect';
-import CartItem from '../../components/cart-item/cart-item.component';
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
@@ -27,8 +27,9 @@ const CheckoutPage = ({cartItems, total}) => (
             </div>
         </div>
         {
-            cartItems.map(cartItem => 
-                cartItem.name)
+            cartItems.map(cartItem => (
+                <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+            ))
         }
         <div className='total'>
             <span> UKUPNO: {total} KM</span>
