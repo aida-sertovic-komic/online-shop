500fe435e2a854e2b66bd6447deb404145f69848
Aida Sertovic <sertovicaida@gmail.com>
2021-05-17 11:55:31 +0200

    Style checkout page

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,8 +3,24 @@ import React from 'react';
 import './checkout.styles.scss';
 
 const CheckoutPage = () => (
-    <div>
-        CHECKOUT PAGE
+    <div className='checkout-page'>
+        <div className='checkout-header'>
+            <div className='header-block'>
+                <span>Proizvod</span>
+            </div>
+            <div className='header-block'>
+                <span>Opis</span>
+            </div>
+            <div className='header-block'>
+                <span>Količina</span>
+            </div>
+            <div className='header-block'>
+                <span>Cijena</span>
+            </div>
+            <div className='header-block'>
+                <span>Ukloni</span>
+            </div>
+        </div>
     </div>
 );
 
