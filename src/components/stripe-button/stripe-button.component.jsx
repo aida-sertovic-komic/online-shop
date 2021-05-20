2c441431f28958f0faf9b8edbd9f5db761331c7d
Aida Sertovic <sertovicaida@gmail.com>
2021-05-20 23:39:47 +0200

    Stripe integration

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51ItCQ5DamlmUOLs3E2Y5kyUArfHPoQMNsdwKyBrX0ZqlIb5Wv8b0uByKfT0NJrG1yCPWEJrTGdLo4CncMYeVa2XJ0056XaVjfh';
+
+   const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    }
+    return(
+        <StripeCheckout 
+            label='Plaćanje'
+            name='E-commerce shop'   
+            billingAddress
+            shippingAddress
+            image='' 
+            description={`Ukupno: ${price} KM` }
+            amount={priceForStripe}
+            currency='BAM'
+            panelLabel='Plaćanje'
+            token={onToken}
+            stripeKey={publishableKey}
+
+        />
+    );
+};
+
+export default StripeCheckoutButton;
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector} from 'reselect';
+
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
@@ -32,8 +34,8 @@ const CheckoutPage = ({cartItems, total}) => (
             ))
         }
         <div className='total'>
-            <span> UKUPNO: {total} KM</span>
-        </div>
+           UKUPNO: {total} KM</div>
+        <StripeCheckoutButton price={total} />
         
     </div>
 );
