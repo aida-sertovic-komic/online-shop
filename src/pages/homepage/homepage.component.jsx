093d67460e48fa2d3681ac1035cda80c4c0419ad
Aida Sertovic <sertovicaida@gmail.com>
2021-07-02 00:14:06 +0200

    Add benefits on homepage

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import Directory from '../../components/directory/directory.component';
 import CustomButton from '../../components/custom-button/custom-button.component';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import coverPhoto from './coverPhoto.jpg';
+import clock from './clock.png';
+import creditCard from './creditCard.png';
+import freeShipping from './freeShipping.png';
 import './homepage.styles.scss';
 
 const HomePage = () => (
@@ -29,6 +32,23 @@ const HomePage = () => (
             <h1>Kategorije</h1>
         </div>
         <Directory />
+        <div className='benefits'>
+            <div>
+                <img src={freeShipping} alt='freeShipping' />
+                <h2>BESPLATNA DOSTAVA</h2>
+            </div>
+            <div>
+                <img src={clock} alt='clock' />
+                <h2>ISPORUKA ZA 2 DANA</h2>
+            </div>
+            <div>
+                <img src={creditCard} alt='creditCard' />
+                <h2>KARTIČNO PLAĆANJE</h2>
+            </div>
+        </div>
+        <div className='popular-products'>
+            <h1>Izdvajamo iz ponude</h1>
+        </div>
     </div>
 );
 export default HomePage;
