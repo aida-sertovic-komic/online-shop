c5f6ad6b363fda638e5f95c63e630b3ac13bc4fe
Aida Sertovic <sertovicaida@gmail.com>
2021-07-01 17:18:41 +0200

    Add style at homepage

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import Directory from '../../components/directory/directory.component';
+import CustomButton from '../../components/custom-button/custom-button.component';
+import coverPhoto from './coverPhoto.jpg';
 import './homepage.styles.scss';
 
 const HomePage = () => (
-    <div className='homepage'>      
+
+    <div className='homepage'>
+        <img src={coverPhoto} alt='cover' />
+        <div className='overlay'>
+            <table>
+                <tbody>
+                    <tr>
+                        <td className='firstColumn'></td>
+                        <td className='secondColumn'>
+                            <div className='gift-shop'>
+                                <h1>Gift Shop</h1>
+                                <CustomButton>POGLEDAJ PONUDU</CustomButton>
+                            </div>
+                        </td>
+                        <td className='third'></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
         <Directory />
     </div>
 );
